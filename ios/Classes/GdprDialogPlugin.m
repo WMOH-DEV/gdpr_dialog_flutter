cf2737d270bac6c90193611379c39bdf4b579a8a
WaeL M.El-Saied <58665715+WMOH-DEV@users.noreply.github.com>
2026-03-11 14:06:03 +0000

    fix: use auto-generated Swift bridging header import in ObjC file

--- ios/Classes/GdprDialogPlugin.m
+++ ios/Classes/GdprDialogPlugin.m
@@ -1,5 +1,10 @@
 #import "GdprDialogPlugin.h"
-#import "SwiftGdprDialogPlugin.h"
+
+#if __has_include(<gdpr_dialog/gdpr_dialog-Swift.h>)
+#import <gdpr_dialog/gdpr_dialog-Swift.h>
+#else
+#import "gdpr_dialog-Swift.h"
+#endif
 
 @implementation GdprDialogPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
